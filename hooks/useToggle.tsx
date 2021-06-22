b8893f3e946983f7b9f1a10b35e4f85726f24e0e
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-06-22 18:57:08 +0530

    feat: CU-3xb9vp: Dark and light mode switch

---
+++ hooks/useToggle.tsx
@@ -0,0 +1,13 @@
+import { useCallback, useState } from 'react';
+
+// Parameter is the boolean, with default "false" value
+const useToggle = (initialState: boolean = false): [boolean, any] => {
+  // Initialize the state
+  const [state, setState] = useState<boolean>(initialState);
+  // Define and memorize toggler function in case we pass down the component,
+  // This function change the boolean value to it's opposite value
+  const toggle = useCallback((): void => setState((prevState) => !prevState), []);
+  return [state, toggle];
+};
+
+export default useToggle;
