db0bc0db402e2846f07b2c658858bbed16213006
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2021-08-08 00:55:06 +0530

    feat: CU-4dddjc: Added toggle button group for preview and code

--- components/TemplateContainer/index.tsx
+++ components/TemplateContainer/index.tsx
@@ -1,5 +1,8 @@
-import { Card, CardHeader, CardContent } from '@material-ui/core';
+import { Card, CardHeader, CardContent, Button, ButtonGroup } from '@material-ui/core';
 import { useTheme } from '@material-ui/core';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import CodeIcon from '@material-ui/icons/Code';
+import useToggle from 'hooks/useToggle';
 
 export type Props = {
   content?: React.ReactNode;
@@ -7,13 +10,35 @@ export type Props = {
 
 export default function TemplateContainer({ content }: Props): JSX.Element {
   const theme = useTheme();
+  const [isPreviewMode, setIsPreviewMode] = useToggle(true);
+
+  const handleModeChange = (enablePreview: boolean): void => {
+    setIsPreviewMode(enablePreview);
+  };
+
   return (
     <Card variant="outlined">
       <CardHeader
-        title=""
-        disableSpacing={true}
+        title={
+          <ButtonGroup variant="outlined" disableElevation size="small">
+            <Button
+              startIcon={<VisibilityIcon />}
+              onClick={() => handleModeChange(true)}
+              variant={isPreviewMode ? 'contained' : 'outlined'}
+            >
+              Preview
+            </Button>
+            <Button
+              startIcon={<CodeIcon />}
+              onClick={() => handleModeChange(false)}
+              variant={isPreviewMode ? 'outlined' : 'contained'}
+            >
+              Code
+            </Button>
+          </ButtonGroup>
+        }
         style={{ borderBottom: `1px solid ${theme.palette.grey[600]}` }}
-      ></CardHeader>
+      />
       <CardContent>{content}</CardContent>
     </Card>
   );
